255701d865905b760a79e8922dd1d7de737a14c6
Ador <balazsik.adorjan@gmail.com>
2022-11-10 22:34:35 +0100

    refactor & add FileList

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,38 @@
 <script lang="ts">
-    import ParamTable from "./components/ParamTable.svelte";
+	import ParamTable from "./components/ParamTable.svelte";
 	import type { IParamTable, IProcessInfo } from "./types";
-    import ProcessInfoTable from "./components/ProcessInfoTable.svelte";
-    import { service } from "./service";
-	
-	let parametersTable: IParamTable = {rows: [], keys: []};
+	import ProcessInfoTable from "./components/ProcessInfoTable.svelte";
+	import { service } from "./service";
+    import FileList from "./components/FileList.svelte";
+
+	let parametersTable: IParamTable = { rows: [], keys: [] };
 	let processInfos: IProcessInfo[] = [];
+	let files = [];
+
+	function loadParameters() {
+		service.load().then((response) => {
+			if (response.ok) {
+				response.json().then((result) => {
+					const keys = Object.keys(result[0]);
+					parametersTable = { rows: result, keys: keys };
+				});
+			}
+		});
+	}
+
+	function loadFiles() {
+		service.listfiles().then(response => {
+            if (response.ok) {
+                response.json().then(result => {
+                    files = result;
+                });
+            }
+        });
+	}
 
 	function load() {
-		service.load().then(
-			(response) => {
-				if (response.ok) {
-					response.json().then((result) => {
-						const keys = Object.keys(result[0]);
-						parametersTable = {rows: result, keys: keys};
-					});
-				}
-			}
-		);
+		loadParameters();
+		loadFiles();
 	}
 
 	function save() {
@@ -27,7 +42,6 @@
 			}
 		});
 	}
-
 </script>
 
 <div id="main">
@@ -36,42 +50,19 @@
 		<h4>Edit Parameters:</h4>
 		<div class="paramButton" on:click={save}>Save</div>
 		<div class="paramButton" on:click={load}>Load</div>
-	  
 	</div>
 	<div id="container_2">
-		<ParamTable data={parametersTable}></ParamTable>
+		<ParamTable data={parametersTable} />
 	</div>
-	
 
-	<div id="container_3"> <h4>View Files: </h4></div>
+	<div id="container_3"><h4>View Files:</h4></div>
 	<div id="container_4">
-		<div id="container_4_1">
-			<table class="table">
-				<thead>
-					<tr><th class="header-label">Folders</th></tr>
-				</thead>
-				<tr><td>Load to DB</td></tr>
-				<tr><td>Logs</td></tr>
-				<tr><td>Output / Biased List</td></tr>
-				<tr><td>Output / Imdb List</td></tr>
-				<tr><td>Output / Omdb List</td></tr>
-			</table>
-		</div>
-		<div id="container_4_2">
-			<table class="table">
-				<thead>
-					<tr><th class="header-label">Files</th></tr>
-				</thead>
-				<tr><td>filmlist_imdbid_12</td></tr>
-				<tr><td>filmlist_imdbid_13</td></tr>
-				<tr><td>filmlist_imdbid_999</td></tr>    
-			</table>
-		</div>
+		<FileList {files}></FileList>
 	</div>
-	<div id="container_5">  </div>
-	<div id="container_6"> 
-		<ProcessInfoTable {processInfos}></ProcessInfoTable>	
+	<div id="container_5" />
+	<div id="container_6">
+		<ProcessInfoTable {processInfos} />
 	</div>
-	
-	<div id="container_7"> MM - 2022 </div>
+
+	<div id="container_7">MM - 2022</div>
 </div>
---
+++ src/components/FileList.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    export let files = [];
+</script>
+
+<div>
+    <div id="container_4_1">
+        <table class="table">
+            <thead>
+                <tr><th class="header-label">Folders</th></tr>
+            </thead>
+            <tr><td>Load to DB</td></tr>
+            <tr><td>Logs</td></tr>
+            <tr><td>Output / Biased List</td></tr>
+            <tr><td>Output / Imdb List</td></tr>
+            <tr><td>Output / Omdb List</td></tr>
+        </table>
+    </div>
+    <div id="container_4_2">
+        <table class="table">
+            <thead>
+                <tr><th class="header-label">Files</th></tr>
+            </thead>
+            {#each files as file}
+                <tr><td>{file.name}</td></tr>
+            {/each}
+        </table>
+    </div>
+</div>
